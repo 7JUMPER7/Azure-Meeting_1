1254955c2ae3b7d45a73547a321f3f10c45f3d51
Arsenii Semenov <2014.semenow.senya@gmail.com>
2021-10-21 21:35:55 +0300

    Initial commit

---
+++ AzureStorageExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Storage;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System.IO;
+
+namespace AzureStorageExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string connStr = "CONSTR";
+
+            BlobServiceClient blobService = new BlobServiceClient(connStr);
+            // string containerName = "cloudwork";
+            // BlobContainerClient container = blobService.GetBlobContainerClient(containerName);
+            // await foreach(var item in container.GetBlobsAsync())
+            // {
+            //     Console.WriteLine($"{item.Name}");
+            //     BlobClient blob = container.GetBlobClient(item.Name);
+            //     Console.WriteLine($"{blob.Uri.AbsoluteUri}");
+            // }
+            string containerName2 = "home";
+            string path = "Data";
+            string filename = "logo.png";
+            string fullPath = Path.Combine(path, filename);
+            BlobContainerClient container2 = new BlobContainerClient(connStr, containerName2);
+            await container2.CreateIfNotExistsAsync(PublicAccessType.Blob);
+            BlobClient blob1 = container2.GetBlobClient(filename);
+            var res = await blob1.UploadAsync(fullPath);
+            System.Console.WriteLine(res.GetRawResponse().Status);
+            System.Console.WriteLine(res.GetRawResponse().ReasonPhrase);
+        }
+    }
+}
---
+++ LR_HW_Semenov_2110/LR_HW_Semenov_2110/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using LR_HW_Semenov_2110.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LR_HW_Semenov_2110.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly Uploader uploader;
+
+        public HomeController(ILogger<HomeController> logger, Uploader uploader)
+        {
+            _logger = logger;
+            this.uploader = uploader;
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await uploader.GetImages());
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(IFormFile image)
+        {
+            if(!await uploader.AddImage(image.FileName, ImageToByteArray(image)))
+            {
+                return View();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public byte[] ImageToByteArray(IFormFile image)
+        {
+            using (var ms = new MemoryStream())
+            {
+                image.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+    }
+}
